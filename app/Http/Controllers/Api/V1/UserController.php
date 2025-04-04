455fa5fcd2f2e62b168513e9f9aaab32ca22be2e
Nay Min Zaw <minzaw.nay@orangekloud.com>
2025-04-04 16:15:11 +0800

    Add get all users route

---
+++ app/Http/Controllers/Api/V1/UserController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\Api\V1\UserResource;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $users = User::all();
+        $usersResource = UserResource::collection($users);
+        return json_encode($usersResource);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -5,11 +5,16 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::group(['prefix' => 'v1'], function () {
-    Route::post('/transaction/create',[\App\Http\Controllers\Api\V1\TransactionController::class,'store']);
-    Route::get('/transaction/{id}',[\App\Http\Controllers\Api\V1\TransactionController::class,'show']);
-    Route::get('/transaction',[\App\Http\Controllers\Api\V1\TransactionController::class,'index']);
-    Route::put('/transaction/{id}',[\App\Http\Controllers\Api\V1\TransactionController::class,'update']);
-    Route::delete('/transaction/{id}',[\App\Http\Controllers\Api\V1\TransactionController::class,'destroy']);
+    Route::group(['prefix' => 'transaction'], function () {
+        Route::post('/create',[\App\Http\Controllers\Api\V1\TransactionController::class,'store']);
+        Route::get('/{id}',[\App\Http\Controllers\Api\V1\TransactionController::class,'show']);
+        Route::get('/',[\App\Http\Controllers\Api\V1\TransactionController::class,'index']);
+        Route::put('/{id}',[\App\Http\Controllers\Api\V1\TransactionController::class,'update']);
+        Route::delete('/{id}',[\App\Http\Controllers\Api\V1\TransactionController::class,'destroy']);
+    });
+    Route::group(['prefix' => 'user'], function () {
+        Route::get('/',[\App\Http\Controllers\Api\V1\UserController::class,'index']);
+    });
 });
 
 Route::get('/user', function (Request $request) {
